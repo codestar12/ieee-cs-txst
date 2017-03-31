af7fb5e141f1be4f225def872b9453857a158c26
Cole Dube <dube1503@gmail.com>
2017-03-30 21:28:56 -0500

    Unified controller support for servo and DC test

--- car-project/controller.py
+++ car-project/controller.py
@@ -1,29 +1,43 @@
 from inputs import *
 import sys
 from gpiozero import AngularServo
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 import time
+import atexit
+
+mh = Adafruit_MotorHAT(addr=0x60)
+leftMotor = mh.getMotor(4)
+#rightMotor = mh.getMoto(3)
 
 servo = AngularServo(
 	pin=17,
 	min_pulse_width = .0008,
 	max_pulse_width = .0018,
-	min_angle = 60,
+	min_angle = -60,
 	max_angle = 60
 )
 
 pad = devices.gamepads[0]
 stick_max = 2.0**15
 
+def turnoffMotors():
+	mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnoffMotors)
+
 def turn(event):
-	if abs(event.state/stick_max) < .1:
+	if abs(event.state/stick_max) < .15:
 		servo.angle = 0
 	else:
 		servo.angle = -servo.min_angle*event.state/stick_max
 	
 	if event.state > 0:
-		print(':100 meters right 4', event.state/stick_max)
+		print('right', event.state/stick_max)
 	else:
-		print(':NASCAR', event.state/stick_max)
+		print('left', event.state/stick_max)
 
 def kill(event):
 	if event.state == 0:
@@ -41,27 +55,41 @@ def drift(event):
 	if event.state:
 		print('tokyo drift')
 
+def forward(event):
+	if event.state:
+		print("forward",event.state)
+		leftMotor.run(Adafruit_MotorHAT.FORWARD)
+#		rightMotor.run(Adafruit_MotorHAT.FORWARD)
+		leftMotor.setSpeed(100)
+#		rightMotor.setSpeed(event.state)
+
+def backward(event):
+	if event.state:
+		print("backward",event.state)
+
 handler = {
 	'ABS_X': turn,
 	'BTN_START': kill,
 	'BTN_EAST': brake,
 	'BTN_SOUTH': gas,
-	'ABS_Z': drift
+#	'ABS_Z': drift,
+	'ABS_RZ': forward,
+#	'ABS_Z': backward
 }
 
 # servo.mid()
 
-print('frame_width:', servo.frame_width)
-print('pulse_width:', servo.pulse_width)
-print('max_pulse_width:', servo.max_pulse_width)
-print('min_pulse_width:', servo.min_pulse_width)
-print('source:', servo.source)
-print('source_delay:', servo.source_delay)
+#print('frame_width:', servo.frame_width)
+#print('pulse_width:', servo.pulse_width)
+#print('max_pulse_width:', servo.max_pulse_width)
+#print('min_pulse_width:', servo.min_pulse_width)
+#print('source:', servo.source)
+#print('source_delay:', servo.source_delay)
 
 while True:
 	events = get_gamepad()
 	for event in events:
 			if event.code in handler:
 				handler[event.code](event)
-			# else:
-			# 	print(event.ev_type, event.code, event.state)
+#	 		else:
+#	 		 	print(event.ev_type, event.code, event.state)
