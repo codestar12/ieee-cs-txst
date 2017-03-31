6b19cd8554175d1045d2110afc623547275678c3
Cole Dube <dube1503@gmail.com>
2017-03-30 19:53:45 -0500

    proper DC motorcode file addition

---
+++ car-project/motorcode.py
@@ -0,0 +1,68 @@
+
+# commands
+
+#git clone https://github.com/adafruit/Adafruit-Motor-HAT-Python-Library.git
+#cd Adafruit-Motor-HAT-Python-Library
+
+#sudo apt-get install python-dev
+
+#sudo python setup.py install
+
+# code
+
+#!/usr/bin/python
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+
+import time
+import atexit
+
+# create a default object, no changes to I2C address or frequency
+mh = Adafruit_MotorHAT(addr=0x60)
+
+# recommended for auto-disabling motors on shutdown!
+def turnOffMotors():
+	mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+	mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+atexit.register(turnOffMotors)
+
+# makes a motor object 3 corresponds to the terminal number (1-4)
+myMotor = mh.getMotor(3)
+
+# set the speed to start, from 0 (off) to 255 (max speed)
+myMotor.setSpeed(150)
+
+
+# example of running code
+while (True):
+	print "Forward! "
+	myMotor.run(Adafruit_MotorHAT.FORWARD)
+
+	print "\tSpeed up..."
+	for i in range(255):
+		myMotor.setSpeed(i)
+		time.sleep(0.01)
+
+	print "\tSlow down..."
+	for i in reversed(range(255)):
+		myMotor.setSpeed(i)
+		time.sleep(0.01)
+
+	print "Backward! "
+	myMotor.run(Adafruit_MotorHAT.BACKWARD)
+
+	print "\tSpeed up..."
+	for i in range(255):
+		myMotor.setSpeed(i)
+		time.sleep(0.01)
+
+	print "\tSlow down..."
+	for i in reversed(range(255)):
+		myMotor.setSpeed(i)
+		time.sleep(0.01)
+
+	print "Release"
+	myMotor.run(Adafruit_MotorHAT.RELEASE)
+	time.sleep(1.0)
